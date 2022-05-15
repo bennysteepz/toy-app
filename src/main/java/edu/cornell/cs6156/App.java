624eff5962559c91420c19fcb58505a2de1b39e0
Benjamin Steeper <bds238@cornell.edu>
2022-05-15 19:39:20 -0400

    change to app.java

--- src/main/java/edu/cornell/cs6156/App.java
+++ src/main/java/edu/cornell/cs6156/App.java
@@ -7,7 +7,7 @@ public class App
     public int count (Collection<String> collection) {
         int test = 1;
         test = 2;
-        test = 3;
+        test = 5;
         int count = 0;
         for (String element : collection) {
             count++;
