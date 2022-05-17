84af91db458ed84ad00f0c24a7938f2c63d2197e
Benjamin Steeper <bds238@cornell.edu>
2022-05-17 19:43:42 -0400

    testing changing app2 and app4

--- src/main/java/edu/cornell/cs6156/App2.java
+++ src/main/java/edu/cornell/cs6156/App2.java
@@ -6,7 +6,7 @@ public class App2 {
 
     public int count2 (Collection<String> collection) {
         int test = 1;
-        test = 81;
+        test = 855;
         int count = 0;
         for (String element : collection) {
             count++;
--- src/main/java/edu/cornell/cs6156/App4.java
+++ src/main/java/edu/cornell/cs6156/App4.java
@@ -6,7 +6,7 @@ public class App4 {
 
     public int count4 (Collection<String> collection) {
         int test = 1;
-        test = 81;
+        test = 85;
         int count = 0;
         for (String element : collection) {
             count++;
